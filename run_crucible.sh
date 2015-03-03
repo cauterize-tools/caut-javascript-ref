9cd3d5f052edf03c0127053c66e407e742d8cefa
John Van Enk <vanenkj@gmail.com>
2015-03-02 19:29:35 -0600

    Get things ready for the crucible.

---
+++ run_crucible.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# This is what John uses to test the caut-c11-sync generator locally. You may
+# need to tweak this to fit your local configuration.
+
+./.cabal-sandbox/bin/cauterize-test \
+  crucible --build-cmd="../../.cabal-sandbox/bin/caut-javascript-ref-gen --spec=%s --meta=%m --output=js" \
+           --run-cmd="node js/test_client.js" \
+           --schema-count=1 \
+           --instance-count=10 \
+           --type-count=20 \
+           --enc-size=128
