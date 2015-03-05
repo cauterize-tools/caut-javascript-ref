63c583e3cb1b8e8503302e4b8226f27e5fde9e64
John Van Enk <vanenkj@gmail.com>
2015-03-05 12:52:31 -0600

    Freeze objects and make width and view members rather than methods.

--- data/support/builtin.js
+++ data/support/builtin.js
@@ -1,11 +1,13 @@
+'use strict';
+
 /* Unpack a value described by the `ctor` object from `u8buf` starting at
  * `offset`. */
 function unpack(ctor, u8buf, offset) {
   var safeOffset = offset || 0;
-  var view = ctor.view();
-  var width = ctor.width();
+  var view = ctor.view;
+  var width = ctor.width;
 
-  if (view.BYTES_PER_ELEMENT != width) {
+  if (view.BYTES_PER_ELEMENT !== width) {
     throw new Error("View/width mismatch. Width is: " + width + ". " +
                     "View bytes per element is: " + view.BYTES_PER_ELEMENT + ".");
   }
@@ -29,7 +31,7 @@ function unpack(ctor, u8buf, offset) {
  * `view` on the `ctor` object must be one of Uint32Array or Int32Array. */
 function unpack64(ctor, u8buf, offset) {
   var safeOffset = offset || 0;
-  var view = ctor.view();
+  var view = ctor.view;
 
   if (8 + safeOffset > u8buf.byteLength) {
     throw new Error("Insufficient bytes: " + u8buf.byteLength.toString());
@@ -55,7 +57,7 @@ function unpack64(ctor, u8buf, offset) {
   return new ctor(new view(b0)[0], new view(b1)[0]);
 };
 
-Bool = function (v) { this.value = v; };
+function Bool(v) { this.value = v; };
 Bool.width = function () { return 1; }
 Bool.view = function () { return Uint8Array; }
 Bool.mk = function (u8buf, offset) {
@@ -66,82 +68,96 @@ Bool.mk = function (u8buf, offset) {
     return new Bool(b.value);
   }
 };
+Object.freeze(Bool);
 exports.Bool = Bool;
 
-U8 = function (v) { this.value = v; };
-U8.width = function () { return 1 };
-U8.view = function () { return Uint8Array; };
+function U8(v) { this.value = v; };
+U8.width = 1;
+U8.view = Uint8Array;
 U8.mk = function (u8buf, offset) { return unpack(U8, u8buf, offset); };
+Object.freeze(U8);
 exports.U8 = U8;
 
-U16 = function (v) { this.value = v; };
-U16.width = function () { return 2; }
-U16.view = function () { return Uint16Array; };
+function U16(v) { this.value = v; };
+U16.width = 2;
+U16.view = Uint16Array;
 U16.mk = function (u8buf, offset) { return unpack(U16, u8buf, offset); };
+Object.freeze(U16);
 exports.U16 = U16;
 
-U32 = function (v) { this.value = v; };
-U32.width = function () { return 4; };
-U32.view = function () { return Uint32Array; };
+function U32(v) { this.value = v; };
+U32.width = 4;
+U32.view = Uint32Array;
 U32.mk = function (u8buf, offset) { return unpack(U32, u8buf, offset); };
+Object.freeze(U32)
 exports.U32 = U32;
 
-U64 = function (v0, v1) { this.v0 = v0; this.v1 = v1; };
-U64.width = function () { return 8; };
-U64.view = function () { return Uint32Array; };
+function U64(v0, v1) { this.v0 = v0; this.v1 = v1; };
+U64.width = 8;
+U64.view = Uint32Array;
 U64.mk = function (u8buf, offset) { return unpack64(U64, u8buf, offset); };
+Object.freeze(U64)
 exports.U64 = U64;
 
-S8 = function (v) { this.value = v; };
-S8.width = function () { return 1 };
-S8.view = function () { return Int8Array; };
+function S8(v) { this.value = v; };
+S8.width = 1;
+S8.view = Int8Array;
 S8.mk = function (u8buf, offset) { return unpack(S8, u8buf, offset); };
+Object.freeze(S8)
 exports.S8 = S8;
 
-S16 = function (v) { this.value = v; };
-S16.width = function () { return 2 };
-S16.view = function () { return Int16Array; };
+function S16(v) { this.value = v; };
+S16.width = 2;
+S16.view = Int16Array;
 S16.mk = function (u8buf, offset) { return unpack(S16, u8buf, offset); };
+Object.freeze(S16)
 exports.S16 = S16;
 
-S32 = function (v) { this.value = v; };
-S32.width = function () { return 4 };
-S32.view = function () { return Int32Array; };
+function S32(v) { this.value = v; };
+S32.width = 4;
+S32.view = Int32Array;
 S32.mk = function (u8buf, offset) { return unpack(S32, u8buf, offset); };
+Object.freeze(S32)
 exports.S32 = S32;
 
-S64 = function (v0, v1) { this.v0 = v0; this.v1 = v1; };
-S64.width = function () { return 8; }
-S64.view = function () { return Uint32Array; };
+function S64(v0, v1) { this.v0 = v0; this.v1 = v1; };
+S64.width = 8;
+S64.view = Uint32Array;
 S64.mk = function (u8buf, offset) { return unpack64(S64, u8buf, offset); };
+Object.freeze(S64)
 exports.S64 = S64
 
-F32 = function (v) { this.value = v; };
-F32.width = function () { return 4 };
-F32.view = function () { return Float32Array; };
+function F32(v) { this.value = v; };
+F32.width = 4;
+F32.view = Float32Array;
 F32.mk = function (u8buf, offset) { return unpack(F32, u8buf, offset); };
+Object.freeze(F32)
 exports.F32 = F32;
 
-F64 = function (v) { this.value = v; };
-F64.width = function () { return 8 };
-F64.view = function () { return Float64Array; };
+function F64(v) { this.value = v; };
+F64.width = 8;
+F64.view = Float64Array;
 F64.mk = function (u8buf, offset) { return unpack(F64, u8buf, offset); };
+Object.freeze(F64)
 exports.F64 = F64;
 
-CU8 = function (v) { this.value = v; };
-CU8.width = function () { return 1 };
-CU8.view = function () { return Uint8Array; };
+function CU8(v) { this.value = v; };
+CU8.width = 1;
+CU8.view = Uint8Array;
 CU8.mk = function (u8buf, offset) { return unpack(CU8, u8buf, offset); };
+Object.freeze(CU8)
 exports.CU8 = CU8;
 
-CU16 = function (v) { this.value = v; };
-CU16.width = function () { return 2; }
-CU16.view = function () { return Uint16Array; };
+function CU16(v) { this.value = v; };
+CU16.width = 2;
+CU16.view = Uint16Array;
 CU16.mk = function (u8buf, offset) { return unpack(CU16, u8buf, offset); };
+Object.freeze(CU16)
 exports.CU16 = CU16;
 
-CU32 = function (v) { this.value = v; };
-CU32.width = function () { return 4; };
-CU32.view = function () { return Uint32Array; };
+function CU32(v) { this.value = v; };
+CU32.width = 4;
+CU32.view = Uint32Array;
 CU32.mk = function (u8buf, offset) { return unpack(CU32, u8buf, offset); };
+Object.freeze(CU32)
 exports.CU32 = CU32;
