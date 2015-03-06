5b28d422feae86a2edaf19d9f664a4c0e3746f91
John Van Enk <vanenkj@gmail.com>
2015-03-06 14:22:37 -0600

    Packers and unpackers done for builtins.

--- data/support/builtin.js
+++ data/support/builtin.js
@@ -13,7 +13,7 @@ function unpack(ctor, u8buf, offset) {
                     "View bytes per element is: " + view.BYTES_PER_ELEMENT + ".");
   }
 
-  if (width > u8buf.byteLength) {
+  if (width + offset > u8buf.byteLength) {
     throw new Error("Insufficient bytes: " + u8buf.byteLength.toString());
   }
 
@@ -58,101 +58,185 @@ function unpack64(ctor, u8buf, offset) {
   return new ctor(new view(b0)[0], new view(b1)[0]);
 }
 
-function U8(v) { this.value = v; }
+function pack(obj, u8buf, offset) {
+  var view = obj.constructor.view;
+  var width = obj.constructor.width;
+
+  if (width + offset > u8buf.byteLength) {
+    throw new Error("Insufficient bytes: " + u8buf.byteLength.toString());
+  }
+
+  var ab = new ArrayBuffer(view.BYTES_PER_ELEMENT);
+  var v = new view(ab);
+  var bv = new Uint8Array(ab);
+
+  v[0] = obj.value;
+
+  var i; for (i = 0; i < width; i++) {
+    u8buf[i] = bv[i];
+  }
+
+  return width;
+}
+
+function pack64(obj, u8buf, offset) {
+  if (8 + offset > u8buf.byteLength) {
+    throw new Error("Insufficient bytes: " + u8buf.byteLength.toString());
+  }
+
+  var ab = new ArrayBuffer(8);
+  var v = new Uint32Array(ab);
+  var bv = new Uint8Array(ab);
+
+  v[0] = obj.v0;
+  v[1] = obj.v1;
+
+  var i; for (i = 0; i < 8; i++) {
+    u8buf[i] = bv[i];
+  }
+}
+
+function U8(v) {
+  this.value = v;
+  this.pack = function (u8buf, offset) { return pack(this, u8buf, offset); };
+}
 U8.width = 1;
 U8.view = Uint8Array;
-U8.mk = function (u8buf, offset) { return unpack(U8, u8buf, offset); };
+U8.unpack = function (u8buf, offset) { return unpack(U8, u8buf, offset); };
 Object.freeze(U8);
 exports.U8 = U8;
 
-function U16(v) { this.value = v; }
+function U16(v) {
+  this.value = v;
+  this.pack = function (u8buf, offset) { return pack(this, u8buf, offset); };
+}
 U16.width = 2;
 U16.view = Uint16Array;
-U16.mk = function (u8buf, offset) { return unpack(U16, u8buf, offset); };
+U16.unpack = function (u8buf, offset) { return unpack(U16, u8buf, offset); };
 Object.freeze(U16);
 exports.U16 = U16;
 
-function U32(v) { this.value = v; }
+function U32(v) {
+  this.value = v;
+  this.pack = function (u8buf, offset) { return pack(this, u8buf, offset); };
+}
 U32.width = 4;
 U32.view = Uint32Array;
-U32.mk = function (u8buf, offset) { return unpack(U32, u8buf, offset); };
+U32.unpack = function (u8buf, offset) { return unpack(U32, u8buf, offset); };
 Object.freeze(U32);
 exports.U32 = U32;
 
-function U64(v0, v1) { this.v0 = v0; this.v1 = v1; }
+function U64(v0, v1) {
+  this.v0 = v0; this.v1 = v1;
+  this.pack = function (u8buf, offset) { return pack64(this, u8buf, offset); };
+}
 U64.width = 8;
 U64.view = Uint32Array;
-U64.mk = function (u8buf, offset) { return unpack64(U64, u8buf, offset); };
+U64.unpack = function (u8buf, offset) { return unpack64(U64, u8buf, offset); };
 Object.freeze(U64);
 exports.U64 = U64;
 
-function S8(v) { this.value = v; }
+function S8(v) {
+  this.value = v;
+  this.pack = function (u8buf, offset) { return pack(this, u8buf, offset); };
+}
 S8.width = 1;
 S8.view = Int8Array;
-S8.mk = function (u8buf, offset) { return unpack(S8, u8buf, offset); };
+S8.unpack = function (u8buf, offset) { return unpack(S8, u8buf, offset); };
 Object.freeze(S8);
 exports.S8 = S8;
 
-function S16(v) { this.value = v; }
+function S16(v) {
+  this.value = v;
+  this.pack = function (u8buf, offset) { return pack(this, u8buf, offset); };
+}
 S16.width = 2;
 S16.view = Int16Array;
-S16.mk = function (u8buf, offset) { return unpack(S16, u8buf, offset); };
+S16.unpack = function (u8buf, offset) { return unpack(S16, u8buf, offset); };
 Object.freeze(S16);
 exports.S16 = S16;
 
-function S32(v) { this.value = v; }
+function S32(v) {
+  this.value = v;
+  this.pack = function (u8buf, offset) { return pack(this, u8buf, offset); };
+}
 S32.width = 4;
 S32.view = Int32Array;
-S32.mk = function (u8buf, offset) { return unpack(S32, u8buf, offset); };
+S32.unpack = function (u8buf, offset) { return unpack(S32, u8buf, offset); };
 Object.freeze(S32);
 exports.S32 = S32;
 
-function S64(v0, v1) { this.v0 = v0; this.v1 = v1; }
+function S64(v0, v1) {
+  this.v0 = v0; this.v1 = v1;
+  this.pack = function (u8buf, offset) { return pack64(this, u8buf, offset); };
+}
 S64.width = 8;
 S64.view = Uint32Array;
-S64.mk = function (u8buf, offset) { return unpack64(S64, u8buf, offset); };
+S64.unpack = function (u8buf, offset) { return unpack64(S64, u8buf, offset); };
 Object.freeze(S64);
 exports.S64 = S64;
 
-function F32(v) { this.value = v; }
+function F32(v) {
+  this.value = v;
+  this.pack = function (u8buf, offset) { return pack(this, u8buf, offset); };
+}
 F32.width = 4;
 F32.view = Float32Array;
-F32.mk = function (u8buf, offset) { return unpack(F32, u8buf, offset); };
+F32.unpack = function (u8buf, offset) { return unpack(F32, u8buf, offset); };
 Object.freeze(F32);
 exports.F32 = F32;
 
-function F64(v) { this.value = v; }
+function F64(v) {
+  this.value = v;
+  this.pack = function (u8buf, offset) { return pack(this, u8buf, offset); };
+}
 F64.width = 8;
 F64.view = Float64Array;
-F64.mk = function (u8buf, offset) { return unpack(F64, u8buf, offset); };
+F64.unpack = function (u8buf, offset) { return unpack(F64, u8buf, offset); };
 Object.freeze(F64);
 exports.F64 = F64;
 
-function CU8(v) { this.value = v; }
+function CU8(v) {
+  this.value = v;
+  this.pack = function (u8buf, offset) { return pack(this, u8buf, offset); };
+}
 CU8.width = 1;
 CU8.view = Uint8Array;
-CU8.mk = function (u8buf, offset) { return unpack(CU8, u8buf, offset); };
+CU8.unpack = function (u8buf, offset) { return unpack(CU8, u8buf, offset); };
 Object.freeze(CU8);
 exports.CU8 = CU8;
 
-function CU16(v) { this.value = v; }
+function CU16(v) {
+  this.value = v;
+  this.pack = function (u8buf, offset) { return pack(this, u8buf, offset); };
+}
 CU16.width = 2;
 CU16.view = Uint16Array;
-CU16.mk = function (u8buf, offset) { return unpack(CU16, u8buf, offset); };
+CU16.unpack = function (u8buf, offset) { return unpack(CU16, u8buf, offset); };
 Object.freeze(CU16);
 exports.CU16 = CU16;
 
-function CU32(v) { this.value = v; }
+function CU32(v) {
+  this.value = v;
+  this.pack = function (u8buf, offset) { return pack(this, u8buf, offset); };
+}
 CU32.width = 4;
 CU32.view = Uint32Array;
-CU32.mk = function (u8buf, offset) { return unpack(CU32, u8buf, offset); };
+CU32.unpack = function (u8buf, offset) { return unpack(CU32, u8buf, offset); };
 Object.freeze(CU32);
 exports.CU32 = CU32;
 
-function Bool(v) { this.value = v; }
-Bool.width = function () { return 1; };
-Bool.view = function () { return Uint8Array; };
-Bool.mk = function (u8buf, offset) {
+function Bool(v) {
+  if (v !== 0 && v !== 1) {
+    throw new Error("Illegal value for boolean: " + v.toString());
+  }
+
+  this.value = v;
+  this.pack = function (u8buf, offset) { return pack(this, u8buf, offset); };
+}
+Bool.width = 1;
+Bool.view = Uint8Array;
+Bool.unpack = function (u8buf, offset) {
   var b = unpack(U8, u8buf, offset);
   if (b.value !== 0 && b.value !== 1) {
     throw new Error("Illegal value for boolean: " + b.value);
--- data/support/test.js
+++ data/support/test.js
@@ -5,21 +5,26 @@ var b = require('./builtin.js');
 var a = new Uint8Array([0x01,0x02,0x03,0x04
                        ,0x05,0x06,0x07,0x08]);
 
-console.log(b.Bool.mk(a,0));
+function eqTest(ctor, v) {
+  var buf = new Uint8Array(ctor.width);
+  var x = new ctor(v);
+  x.pack(buf,0);
+  var y = ctor.unpack(buf,0);
 
-console.log(b.U8.mk(a,0));
-console.log(b.U16.mk(a,0));
-console.log(b.U32.mk(a,0));
-console.log(b.U64.mk(a,0));
+  if (x.value === y.value) {
+    console.log(ctor.name + " equal");
+  } else {
+    console.log(ctor.name + " not equal");
+  }
+}
 
-console.log(b.CU8.mk(a,0));
-console.log(b.CU16.mk(a,0));
-console.log(b.CU32.mk(a,0));
+var ctors = [b.U8,b.U16,b.U32,b.U64
+            ,b.S8,b.S16,b.S32,b.S64
+            ,b.CU8,b.CU16,b.CU32
+            ,b.F32,b.F64
+            ,b.Bool];
 
-console.log(b.S8.mk(a,0));
-console.log(b.S16.mk(a,0));
-console.log(b.S32.mk(a,0));
-console.log(b.S64.mk(a,0));
-
-console.log(b.F32.mk(a,0));
-console.log(b.F64.mk(a,0));
+var ctor, x;
+var i; for(i = 0; i < ctors.length; i++) {
+  eqTest(ctors[i], 1);
+}
