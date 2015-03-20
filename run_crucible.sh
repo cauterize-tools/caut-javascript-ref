606179152fc65d4cf68fbaa7a42f4fd5f4b6b0ad
John Van Enk <vanenkj@gmail.com>
2015-03-19 17:21:21 -0700

    Use better flags.

--- run_crucible.sh
+++ run_crucible.sh
@@ -3,8 +3,7 @@
 ./.cabal-sandbox/bin/cauterize-test \
   crucible --build-cmd="../../.cabal-sandbox/bin/caut-javascript-ref-gen --spec=%s --meta=%m --output=js" \
            --run-cmd="node js/test_client.js" \
-           --schema-count=1 \
+           --schema-count=10 \
            --instance-count=100 \
-           --type-count=3 \
-           --prototypes=combination \
-           --enc-size=1024
+           --type-count=100 \
+           --enc-size=10240
